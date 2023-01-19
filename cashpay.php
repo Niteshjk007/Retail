3e03b00b23edf0b4f9df547492fc0b2bef7a42b8
Niteshjk007 <119559732+Niteshjk007@users.noreply.github.com>
2023-01-19 17:19:24 +0530

    Add files via upload

---
+++ cashpay.php
@@ -0,0 +1,46 @@
+<?php
+session_start();
+require 'connection.php';
+$conn = Connect();
+if(!isset($_SESSION['login_customer'])){
+    header("location: index.php"); //Redirecting to mystore Page
+}
+if(isset($_SESSION['login_manager'])){
+    header("location: manager.php"); //Redirecting to mystore Page
+}
+if(isset($_SESSION['login_admin'])){
+    header("location: admin.php"); //Redirecting to mystore Page
+}
+?>
+
+<?php
+    $gtotal = 0;
+    foreach($_SESSION["cart"] as $keys => $values) {
+        $total = ($values["quantity"] * $values["price"]);
+        $gtotal = $gtotal + $total;
+    }
+    $item = $_SESSION["cart"][0];
+    $S_ID = $item["S_ID"];
+    $username = $_SESSION["login_customer"];
+    $order_date = date('Y-m-d');
+    $order_status = "PLACED";
+    $delivery_address = $_GET["address"];
+    $query = "INSERT INTO orders (total_price, order_date, order_status, delivery_address, username, S_ID) 
+            VALUES ('" . $gtotal . "','" .  $order_date . "','" . $order_status . "','" . $delivery_address . "','" . $username . "','" . $S_ID . "')";
+
+    $success = $conn->query($query);
+
+    $ordeS_ID = $conn->insert_id;
+    foreach($_SESSION["cart"] as $keys => $values) {
+        $F_ID = $values["product_ID"];
+        $quantity = $values["quantity"];
+        $query = "INSERT INTO order_item (ordeS_ID, product_ID, quantity) 
+                VALUES ('". $ordeS_ID ."','" . $F_ID . "','" . $quantity . "')";
+
+        $success = $conn->query($query); 
+    }
+
+    unset($_SESSION["cart"]);
+
+    echo '<script>window.location="payment_successfull.php?orderid='. $ordeS_ID .'"</script>';
+?>
